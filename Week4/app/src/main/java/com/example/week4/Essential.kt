9cea21d61598130c6c647665ea96390432b3751d
lquyet <anhquyethgvx@gmail.com>
2023-09-12 02:26:49 +0700

    p2

--- Week4/app/src/main/java/com/example/week4/Essential.kt
+++ Week4/app/src/main/java/com/example/week4/Essential.kt
@@ -1,11 +1,31 @@
 package com.example.week4
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.week4.databinding.ActivityEssentialBinding
 
 class Essential : AppCompatActivity() {
+    private lateinit var binding: ActivityEssentialBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_essential)
+        binding = ActivityEssentialBinding.inflate(layoutInflater)
+
+        binding.apply {
+            linearEssential.setOnClickListener {
+                startActivity(Intent(this@Essential, EssentialLinear::class.java))
+            }
+            constraintEssential.setOnClickListener {
+                startActivity(Intent(this@Essential, EssentialConstraint::class.java))
+            }
+            relativeEssential.setOnClickListener {
+                startActivity(Intent(this@Essential, EssentialRelative::class.java))
+            }
+            tableEssential.setOnClickListener {
+                startActivity(Intent(this@Essential, EssentialTable::class.java))
+            }
+        }
+
+        setContentView(binding.root)
     }
 }
